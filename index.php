d60032c5363cca0dc731dccf46c2a60a737d927d
rafsanrjkhan <rjkhan104@gmail.com>
2023-10-17 17:26:39 +0600

    nav css

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jQuery_Ajax </title>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/main.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> -->
 </head>
 <body>
     <nav>
@@ -13,7 +14,10 @@
             <li><a href="#">Downloads</a></li>
             <li><a href="#">Tutorials</a></li>
         </ul>
+        <hr>
     </nav>
+    <!-- <section id="main">Hello</section>
+        <h1>Hellooooo</h1> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->   
 </body>
