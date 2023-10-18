d1e2831852aa255f40657870a1a948498f994d76
rafsanrjkhan <rjkhan104@gmail.com>
2023-10-18 17:53:33 +0600

    nav output console log

--- index.php
+++ index.php
@@ -10,16 +10,30 @@
 <body>
     <nav>
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Downloads</a></li>
-            <li><a href="#">Tutorials</a></li>
+            <li><a href="#" data-page="home">Home</a></li>
+            <li><a href="#" data-page="download">Downloads</a></li>
+            <li><a href="#" data-page="tutorials">Tutorials</a></li>
         </ul>
-        <hr>
     </nav>
     <!-- <section id="main">Hello</section>
         <h1>Hellooooo</h1> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->   
+    <script>
+        var app = {
+            nav: $('nav ul li a')
+        };
+        app.putContent = function() {
+
+        }
+        app.init =function() {
+            app.nav.on('click', function() {
+                var page = $(this).data('page');
+                   console.log(page);
+
+            });
+        }();
+    </script>
 </body>
 </html>
 
