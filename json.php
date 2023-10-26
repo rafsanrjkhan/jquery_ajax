07e2f6d79453dd035a189ea9fbedebc3063bd672
rafsanrjkhan <rjkhan104@gmail.com>
2023-10-26 17:09:39 +0600

    names  results

--- json.php
+++ json.php
@@ -2,6 +2,8 @@
 header('Content-type: application/json');
 ?>
 {
+    "name": 3,
+    "names": ["Rafsan", "Jaman", "Khan"],
     "count": 10,
     "results": ["Result One", "Result Two", "Result Three"]
 }
