287bb87e4ac1ce910dcb1c89f54b1b895a14fe68
rafsanrjkhan <rjkhan104@gmail.com>
2023-10-29 17:48:31 +0600

    search names

--- index.php
+++ index.php
@@ -9,16 +9,26 @@
 <body>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <p id="count"></p>
+    <!-- <p id="count"></p> -->
+    <p id="names"></p>
+
    <script>
         $.ajax({
             url: 'json.php',
             dataType: 'json',
             success: function(data) {
-                $('#count').text('Your search returned ' + data.count + ' results.');
+                $('#names').text('Your search returned ' + data.names + ' results.');
                 // console.log(data.names);
             }
         });
+        // $.ajax({
+        //     url: 'json.php',
+        //     dataType: 'json',
+        //     success: function(data) {
+        //         $('#count').text('Your search returned ' + data.count + ' results.');
+        //         // console.log(data.names);
+        //     }
+        // });
     </script>
 </body>
 </html>
