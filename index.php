474625d03baad26f557b24e3e6fb1491c2ffef59
rafsanrjkhan <rjkhan104@gmail.com>
2023-10-30 17:06:25 +0600

    detailed result output

--- index.php
+++ index.php
@@ -1,24 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jQuery_Ajax </title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> -->
 </head>
+
 <body>
-    
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <!-- <p id="count"></p> -->
-    <p id="names"></p>
+    <p id="count"></p>
+    <div id="results"></div>
 
-   <script>
+    <script>
         $.ajax({
             url: 'json.php',
             dataType: 'json',
             success: function(data) {
-                $('#names').text('Your search returned ' + data.names + ' results.');
-                // console.log(data.names);
+                var count = $('#count'),
+                    results = $('#results');
+
+                if (data.count) {
+                    count.text('Your search returned ' + data.count + ' results.');
+
+
+                    $(data.results).each(function(index, value) {
+                        results.append($('<p></p>').text(value));
+                    });
+                } else {
+                    count.text('No results.');
+                }
             }
         });
         // $.ajax({
@@ -31,6 +45,5 @@
         // });
     </script>
 </body>
+
 </html>
-
-
